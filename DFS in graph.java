35cc94e76ca4990db236fca908d25f52e640ced7
Shambhavi Awasthi <shambhavi020898@gmail.com>
2021-01-31 15:56:23 +0530

    Update DFS in graph.java

--- DFS in graph.java
+++ DFS in graph.java
@@ -1,25 +1,56 @@
-class Traversal
-{
+import java.io.*;
+import java.util.*;
+
+class Graph{
    
-    static ArrayList<Integer> recur(ArrayList<Integer> al,ArrayList<ArrayList<Integer>> g, int N,int []visited,int start){
-       if(visited[start]==0){
-           al.add(start);
-           visited[start]=1;
-       }
-       for(int j=0;j<g.get(start).size();j++){
-           if(visited[g.get(start).get(j)]==0){
-               recur(al,g,N,visited,g.get(start).get(j));
-           }
-       }
-       return al; 
-    }
-    static ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> g, int N)
+   private int V;
+   private LinkedList<Integer> [] adj;
+   
+   Graph(int v){
+      int v=V;
+      adj=new LinkedList[v];
+      for(int i=0;i<v;i++){
+         adj[i]=new LinkedList<Integer>();
+      }
+   }
+   
+   public void addEdge(int startingVertex, int endingVertex){
+      adj[startingVertex].add(endingVertex);
+   }
+   
+   public void DFS(int s){
+      boolean visited[]=new boolean[V];
+      Stack<Integer> st=new Stack<>();
+      st.push(s);
+      visited[s]=true;
+      while(!st.isEmpty()){
+         s=st.peek();
+         st.pop();
+         
+         // if popped element is not visited then print it
+         if(visited[s]==false){
+            System.out.print(s+" ");
+            visited[s]=true;
+         }
+         Iterator<Integer> i=adj[s].iterator();
+         while(i.hasNext()){
+            int p=i.next();
+            if(visited[p]==false)
+               st.push(p);
+         }
+      }
+   }
+   
+   public static void main(String[] args) 
     {
-       // add your code here
-       int visited[]=new int[N];
-       ArrayList<Integer> al=new ArrayList<Integer>();
-  int start=0;
-  return recur(al,g,N,visited,start);
        
+        Graph g = new Graph(5);
+        g.addEdge(1, 0);
+        g.addEdge(0, 2);
+        g.addEdge(2, 1);
+        g.addEdge(0, 3);
+        g.addEdge(1, 4);
+        g.DFS(0);
     }
+   
 }
