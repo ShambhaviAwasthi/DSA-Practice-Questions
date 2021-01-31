3f27ab2b3ad571fa0434d1d1c4f2ab6601a25f6b
Shambhavi Awasthi <shambhavi020898@gmail.com>
2021-01-31 16:03:41 +0530

    Update DFS for graph Recursive.java

--- DFS for graph Recursive.java
+++ DFS for graph Recursive.java
@@ -1,12 +1,8 @@
 
 void DFSUtil(int v, boolean visited[])
     {
-        // Mark the current node as visited and print it
         visited[v] = true;
         System.out.print(v + " ");
- 
-        // Recur for all the vertices adjacent to this
-        // vertex
         Iterator<Integer> i = adj[v].listIterator();
         while (i.hasNext()) 
         {
@@ -15,19 +11,31 @@ void DFSUtil(int v, boolean visited[])
                 DFSUtil(n, visited);
         }
     }
- 
-    // The function to do DFS traversal.
-    // It uses recursive
-    // DFSUtil()
     void DFS(int v)
     {
-        // Mark all the vertices as 
-        // not visited(set as
-        // false by default in java)
         boolean visited[] = new boolean[V];
- 
-        // Call the recursive helper 
-        // function to print DFS
-        // traversal
         DFSUtil(v, visited);
+    }
+
+**********************************************************************************************************************************************************
+
+// for handling not connected components also
+
+void DFSUtil(int v, boolean visited[])
+    {
+        visited[v] = true;
+        System.out.print(v + " ");
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+    void DFS()
+    {
+        boolean visited[] = new boolean[V];
+        for (int i = 0; i < V; ++i)
+            if (visited[i] == false)
+                DFSUtil(i, visited);
     }
